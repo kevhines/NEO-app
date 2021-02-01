f70239d3295709c6d3d609ba8836c025f39e9b0b
Kevin <kevhines@gmail.com>
2021-02-01 16:51:52 -0500

    welcome and get_date classes in CLI. set bin/run to call welcome

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,3 +1,16 @@
 class CLI
     #this class will interface wih the user
+
+    def run_app
+        puts "Welcome to the NEO app"
+        puts "NEO stands for Near Earth Objects. In this case we are talking about Asteroids."
+        self.get_date
+    end
+
+    def get_date
+        puts "Enter a date to see what Asteroids are flying by Earth on that day (mm-dd-yyyy):"
+        input = gets.chomp
+        puts input
+    end
+
 end
